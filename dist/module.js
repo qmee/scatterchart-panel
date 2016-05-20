29902c0da6a3ea2ebca4a9411e8fc121785acac8
Jonathan Knight <jonathan@jrlknight.com>
2016-05-20 16:10:11 +0100

    Initial Commit

---
+++ dist/module.js
@@ -0,0 +1,19 @@
+'use strict';
+
+System.register(['lodash', './scatterchart_ctrl'], function (_export, _context) {
+  "use strict";
+
+  var _, ScatterChartCtrl;
+
+  return {
+    setters: [function (_lodash) {
+      _ = _lodash.default;
+    }, function (_scatterchart_ctrl) {
+      ScatterChartCtrl = _scatterchart_ctrl.ScatterChartCtrl;
+    }],
+    execute: function () {
+      _export('PanelCtrl', ScatterChartCtrl);
+    }
+  };
+});
+//# sourceMappingURL=module.js.map
---
+++ src/module.js
@@ -0,0 +1,6 @@
+import _ from 'lodash';
+import {ScatterChartCtrl} from './scatterchart_ctrl';
+
+export {
+  ScatterChartCtrl as PanelCtrl
+};
